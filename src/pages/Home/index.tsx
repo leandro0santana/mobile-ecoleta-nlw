842ed7a5e3c5784348a9347559e487b02e8dadf8
Leandro C. Santana <leandro.c.santana@hotmail.com>
2020-06-15 17:55:39 -0300

    Finalização do app

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Feather as Icon} from '@expo/vector-icons'
+import { View, ImageBackground, Image, StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const [uf, setUf] = useState('');
+  const [city, setCity] = useState('');
+
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points', {
+      uf,
+      city,
+    });
+  }
+
   return( 
-    <ImageBackground 
-      source={require('../../assets/home-background.png')} 
-      style={styles.container}
-      imageStyle={{ width:274, height:368 }}     
-    > 
-      <View style={styles.main}>
-        <Image source={require('../../assets/logo.png')} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-      </View>
-      <View>
+    <KeyboardAvoidingView 
+      style={{ flex: 1 }} 
+      behavior={Platform.OS === 'ios' ? 'padding': undefined}
+    >
+      <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{ width:274, height:368 }}     
+      > 
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+          </View>
+        </View>
 
-        <View style={styles.footer}></View>
+        <View>
+          <View style={styles.footer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite a UF"
+              value={uf}
+              maxLength={2}
+              autoCapitalize="characters"
+              autoCorrect={false}
+              onChangeText={setUf}
+            />
 
-      </View>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite a cidade"
+              value={city}
+              autoCorrect={false}
+              onChangeText={setCity}
+            />
 
-    </ImageBackground>
+            <RectButton style={styles.button} onPress={handleNavigateToPoints} >
+              <View style={styles.buttonIcon}>
+                <Text>
+                  <Icon name="arrow-right" color="#FFF" size={24} />
+                </Text>
+              </View>
+              <Text style={styles.buttonText}>
+                Entrar
+              </Text>
+            </RectButton>
+          </View>
+        </View>
+
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -27,7 +77,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: '#f0f0f5'
   },
 
   main: {
@@ -55,6 +104,15 @@ const styles = StyleSheet.create({
 
   select:{},
 
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
   button: {
     backgroundColor: '#34CB79',
     height: 60,
@@ -62,6 +120,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     overflow: 'hidden',
     alignItems: 'center',
+    marginTop: 8,
   },
 
   buttonIcon: {
@@ -80,10 +139,6 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto_500Medium',
     fontSize: 16,
   }
-
-
-
-
 
 });
 
